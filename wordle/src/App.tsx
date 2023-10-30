e20871b4e0bf8307ee86bf23cb017871ec20ac1d
Po Lien Lin <polien.joe.lin@gmail.com>
2023-10-30 18:53:29 +0800

    Switch to typescript project

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import reactLogo from "./assets/react.svg";
 import wordleLogo from "/wordle.png";
 
@@ -16,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TypeScript</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
