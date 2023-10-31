37231b930ee9e5b1c6a0249576b666a4722f78da
Po Lien Lin <polien.joe.lin@gmail.com>
2023-10-31 18:19:54 +0800

    Make WordList

--- wordle/src/GameBoard/index.tsx
+++ wordle/src/GameBoard/index.tsx
@@ -1,14 +1,21 @@
+import { useEffect, useState } from "react";
+import WordList from "../assets/wordlist.json";
 import CharRow from "./CharRow";
 
 const GameBoard = () => {
-  const ANSWER: string = "GRAIL";
+  const [answer, setAnswer] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * WordList.length);
+    setAnswer(WordList[randomIndex].toUpperCase());
+  }, []);
 
   const inputWords = [
     "Billy".toUpperCase(),
     "Grace".toUpperCase(),
     "Trail".toUpperCase(),
     "GRAIL".toUpperCase(),
-    "g".toUpperCase(),
+    answer.toUpperCase(),
     "".toUpperCase(),
   ];
 
@@ -16,7 +23,7 @@ const GameBoard = () => {
     <main className="mx-auto flex w-full flex-row justify-center pt-14 text-center">
       <div className="boardGridContainer grid h-[480px] w-[400px] grid-cols-5 grid-rows-6 gap-3">
         {inputWords.map((word, index) => {
-          return <CharRow word={word} answer={ANSWER} key={index} />;
+          return <CharRow word={word} answer={answer} key={index} />;
         })}
       </div>
     </main>
